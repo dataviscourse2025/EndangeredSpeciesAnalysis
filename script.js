1614b627c451b89e2e29f549233c39b51fc00335
victoriamache <victoriajm26@gmail.com>
2025-10-22 13:29:49 -0600

    fixing projection

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ function renderUSMap() {
 
   const projection = d3.geoAlbersUsa()
   .translate([width / 2, height / 2])
-  .scale(width * 1.2); 
+  .scale(1000); 
   const path = d3.geoPath().projection(projection);
 
   Promise.all([
