c8f44c3bd7859e15d77b71ddc9716ca37ebe49ff
victoriamache <victoriajm26@gmail.com>
2025-11-21 12:04:35 -0700

    amendment button

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ function renderStackedArea() {
       .style("padding", "8px 10px")
       .style("box-shadow", "0 2px 6px rgba(0,0,0,0.15)")
       .style("font-size", "16px")
-      .style("pointer-events", "none")
       .style("opacity", 0);
 
     // Title
